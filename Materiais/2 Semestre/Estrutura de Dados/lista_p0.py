e26a90d0bb33408bd1a102c9c676ceec869146eb
Arthur Óliver Rossi Alves <arthuroliverrossialves@gmail.com>
2026-08-17 20:18:30 -0300

    feat: adicionando resolução dos exercícios P0

--- Materiais/2 Semestre/Estrutura de Dados/lista_p0.py
+++ Materiais/2 Semestre/Estrutura de Dados/lista_p0.py
@@ -53,3 +53,44 @@ def separa_n (string):
     return int(indice)
 saida = sorted(list, key=separa_n)
 print(f'7 -',saida)
+
+#Crie um dicionário que mapeia os números de 1 a 10 para seus respectivos quadrados, usando dict comprehension.
+dicionario = {n: n**2 for n in range(1, 11)}
+print(f'8 -', dicionario)
+
+#Dada uma string, crie um dicionário onde as chaves são os caracteres e os valores são a contagem de vezes que cada caractere aparece.
+string = 'arthur'
+dicionario = {letra: string.count(letra) for letra in string}
+print(f'9 -', dicionario)
+
+#Dado um dicionário qualquer, crie um novo dicionário onde as chaves e os valores estejam invertidos.
+dicionario = {'a': 1, 'b': 2, 'c': 3}
+dicionario_final = {numero: letra for letra, numero in dicionario.items()}
+print(f'10 -', dicionario_final)
+
+#Dado um dicionário de números, crie um novo dicionário contendo apenas os pares chave-valor onde o valor seja maior que um determinado número.
+dicionario = {'a': 13, 'b': 10, 'c': 3, 'd': 4}
+chave_valor = 4
+dicionario_final = {letra: valor for letra, valor in dicionario.items() if valor > chave_valor}
+print(f'11 -', dicionario_final)
+
+#Dado um dicionário, ordene-o pelos valores.
+dicionario = {'a': 13, 'b': 10, 'c': 3, 'd': 4}
+dicionario_final = dict(sorted(dicionario.items(), key=lambda item: item[1]))
+print(f'12 -', dicionario_final)
+
+#Dado um dicionário onde as chaves são palavras, ordene-o com base no comprimento das chaves.
+dicionario = {'banana': 1, 'abacaxi': 2, 'uva': 3}
+dicionario_final = dict(sorted(dicionario.items(), key=lambda item: len(item[0])))
+print(f'13 -', dicionario_final)
+
+#Dada uma frase, crie um dicionário onde as chaves são palavras e os valores são a contagem de vezes que cada palavra aparece.
+frase = "abacate no almoco e melhor que abacate na janta".split()
+dicionario = {palavra: frase.count(palavra) for palavra in frase}
+print(f'15 -', dicionario)
+
+#Dado um dicionário onde os valores são números, crie um novo dicionário onde cada valor seja a raiz quadrada do original.
+#TERMINAR!
+
+#Dada uma lista de palavras, crie um dicionário onde as chaves sejam as primeiras letras e os valores sejam listas das palavras correspondentes.
+#TERMINAR!
