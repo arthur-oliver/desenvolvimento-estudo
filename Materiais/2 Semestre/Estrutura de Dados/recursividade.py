9909076e5d2df47aa37d714c3f0cd2afac652cfb
Arthur Óliver Rossi Alves <arthuroliverrossialves@gmail.com>
2026-08-18 12:07:07 -0300

    feat: adicionando conteúdo de recursividade em estrutura de dados

--- Materiais/2 Semestre/Estrutura de Dados/recursividade.py
+++ Materiais/2 Semestre/Estrutura de Dados/recursividade.py
@@ -18,3 +18,31 @@ print (fat(3))
 #    if 1 == 0 or 1 == 1: return 1
 #Acima tenho 3 instâncias de fat, no return 1 saio da última instância,
 #acordo a anterior que faz 2*1, e saio da instância, que acorda a anterior e faz 3*2 = 6, que é o que imprime 6
+
+#EXERCÍCIO
+
+def pot(x, n): #potência de x elevado à n, sem usar **
+  if n == 0: return 1
+  return x * pot(x, n-1) # conceito da matemática: x**n = x * x**(n-1)
+print (pot(2, 3)) #saída é 8
+
+# ----------------------
+
+def inv(s): #inverter uma string 'abacate' ---> 'etacaba'
+   if len(s) == 1: return s
+   return inv(s[1:]) + s[0]
+print(inv('abacate'))
+
+# ----------------------
+
+def sd(n): #soma dos dígitos de n - EX: 123 = 1 + 2 + 3
+   if n <= 9 : return n
+   return n % 10 + sd(n // 10) # // é uma divisão por inteiro
+print(sd(123))
+
+# ----------------------
+
+def fib(n): #somente o número na posição n da sequência de fiboonacci: 1 1 2 3 5 8 13 21 34
+   if n == 1 or n == 2 : return 1
+   return fib(n-1) + fib(n-2)
+print(fib(100)) #saída = 8
