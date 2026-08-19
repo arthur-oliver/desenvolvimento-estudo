2d2b9d9a4dd99002e519e8f1d5cc53d221b97e42
Arthur Óliver Rossi Alves <arthuroliverrossialves@gmail.com>
2026-08-19 11:57:43 -0300

    feat: adicionando conteúdo poo em tec program

---
+++ Materiais/2 Semestre/Técnicas de Programação/poo/js/empresa.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Criar classe
+class Empresa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+exports.default = Empresa;
