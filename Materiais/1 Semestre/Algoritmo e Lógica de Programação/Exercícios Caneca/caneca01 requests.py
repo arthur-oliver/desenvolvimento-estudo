4f9478682b2b5fbfb88b9444140e5e5da78e9d28
Arthur Óliver Rossi Alves <arthuroliverrossialves@gmail.com>
2026-08-31 15:58:46 -0300

    feat: adicionando exercício na matéria BD

--- Materiais/1 Semestre/Algoritmo e Lógica de Programação/Exercícios Caneca/caneca01 requests.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = 'https://fatec-cafe.glitch.me/'
-texto = requests.get(url).text
-print (texto)
-
--- Materiais/1 Semestre/Algoritmo e Lógica de Programação/Exercícios Caneca/caneca03 requests.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = 'https://fatec-cafe.glitch.me/vip'
-texto = requests.get(url).text  
-preço = texto[234:238]
-print (preço)
-
